fc1456f75befff654ed138a2138599a6f98ec4dd
muhdarifrawi <muhdarifrawi@gmail.com>
2020-12-16 21:23:08 +0800

    Fetched Weather API and datetime - able to connect to API - have yet to dive deeper to fetch specific data in API - able to fetch datetime.now() - able to format datetime.now() for easy use

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import requests
+from datetime import datetime, time
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    data = requests.get("https://api.data.gov.sg/v1/environment/24-hour-weather-forecast")
+    # print(data)
+    now = datetime.now().isoformat(timespec="seconds")
+    strp_now = datetime.strptime(str(now),"%Y-%m-%dT%H:%M:%S")
+    print("datetime: ",strp_now)
     return render_template("index.html")
